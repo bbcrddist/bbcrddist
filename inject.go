e50b23c4c79b5a8f8ea5c806a2eb6e5cb9723dce
bbcrddist <bbcrddistara@outlook.com>
2024-07-04 15:17:17 +0100

    Update inject.go

--- inject.go
+++ inject.go
@@ -7,3 +7,16 @@ import (
 	"slices"
 	"sync"
 )
+
+type Injector interface {
+	Get(reflect.Type) (reflect.Value, error)
+}
+
+type Injectable interface {
+	Type() reflect.Type
+	Inject(Injector) (reflect.Value, error)
+}
+
+func New(args ...any) (*Scope, error) {
+	return (*Scope)(nil).Scope(args...)
+}
