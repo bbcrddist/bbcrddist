2b8432813b5b2270f1697c1d35987c38dcffe752
bbcrddist <bbcrddistara@outlook.com>
2024-07-08 14:18:09 +0100

    Create clean_test.go

---
+++ clean_test.go
@@ -0,0 +1,36 @@
+
+
+var _ = Describe("Cleanup", func() {
+	Context("with no cleanup functions registered", func() {
+		It("should do nothing when told to clean up", func() {
+			Cleanup() //ha!
+		})
+	})
+
+	Context("with cleanup functions registered", func() {
+		var calls []string
+		BeforeEach(func() {
+			calls = []string{}
+			Register(func() {
+				calls = append(calls, "A")
+			})
+			Register(func() {
+				calls = append(calls, "B")
+			})
+		})
+
+		It("should call the functions when told to clean up", func() {
+			Cleanup()
+			Ω(calls).Should(Equal([]string{"A", "B"}))
+		})
+
+		It("should unregister the functions upon cleanup", func() {
+			Cleanup()
+			Ω(calls).Should(Equal([]string{"A", "B"}))
+
+			calls = []string{}
+			Cleanup()
+			Ω(calls).Should(BeEmpty())
+		})
+	})
+})
