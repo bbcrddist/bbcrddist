e80a36bc0cad6afdd3600ca8e76850fb6e564600
bbcrddist <bbcrddistara@outlook.com>
2024-07-09 12:43:23 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -29,3 +29,33 @@ variable "key_name" {
   description = "The aws ssh key name."
   default = "p_singapore"
 }
+
+variable "node" {
+  description = "Properties for MachinePool node types"
+  type = object({
+    ctl_plane = map(any)
+    worker    = map(any)
+  })
+}
+
+variable "rancher_env" {
+  description = "Variables for Rancher environment"
+  type = object({
+    cloud_credential    = string
+    cluster_annotations = map(string)
+    cluster_labels      = map(string)
+    rke2_version        = string
+  })
+}
+
+variable "vsphere_env" {
+  description = "Variables for vSphere environment"
+  type = object({
+    cloud_image_name = string
+    datacenter       = string
+    datastore        = string
+    library_name     = string
+    server           = string
+    vm_network       = string
+  })
+}
